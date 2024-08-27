237bf81dc4c424885f40cfd8038c678311ae6bb5
MAC <MAC@Morscino.local>
2024-08-27 08:26:11 +0100

    WIP

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils'
+import AppHeader from '@/components/layouts/AppHeader.vue'
+
+describe('AppHeader.vue', () => {
+  it('Weather App', () => {
+    const siteName = 'Weather App'
+    const wrapper = shallowMount(AppHeader, {
+      data() {
+        return {
+          siteName,
+        }
+      },
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
